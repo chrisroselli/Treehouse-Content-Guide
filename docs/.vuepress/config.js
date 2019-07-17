6f364e9515f5d1a1d33a51a52afa4d5f27258e39
WebProduction <WebProduction@admins-iMac-10.local>
2019-07-17 15:17:37 -0400

    Removed base path

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,6 @@
 module.exports = {
   title: 'Treehouse Content Guide',
   description: 'Treehouse Content Guide',
-  base: '/Treehouse-Content-Guide/',
   themeConfig: {
     displayAllHeaders: true 
   }
