a9d240ffa23c6d837d83951b8f382e6c148a00ff
WebProduction <WebProduction@admins-iMac-10.local>
2019-07-17 16:08:33 -0400

    Updated sidebar config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,7 @@
 module.exports = {
   title: 'Treehouse Content Guide',
-  description: 'Treehouse Content Guide'
+  description: 'Treehouse Content Guide',
+  themeConfig: {
+    sidebar: 'auto'
+  }
 } 
